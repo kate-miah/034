f5ce6dbcde54a108db3bd5b4069a925569335ac2
kate-miah <ekaterin89@gmail.com>
2025-07-13 23:15:27 -0500

    double lope

--- index.js
+++ index.js
@@ -1,29 +1,113 @@
 // всі парні числа від 0 до 10 (включно)
 
-for (let i = 0; i <= 10; i++) {
-    if (i % 2 === 0){
-        console.log(i);
-    }
-}
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 === 0){
+//         console.log(i);
+//     }
+// }
 
 // Task
 
-function getSumOfRange(){
-let num1 = Number(prompt('Введіть перше число'));
-let num2 = Number(prompt('Введіть друге число'));
+// function getSumOfRange(){
+// let num1 = Number(prompt('Введіть перше число'));
+// let num2 = Number(prompt('Введіть друге число'));
 
-if (num2 > num1) {
+// if (num2 > num1) {
     
-    console.log(sum)
-} else {
-    console.log('Неправильні вхідні дані')
-}
-}
+//     console.log(sum)
+// } else {
+//     console.log('Неправильні вхідні дані')
+// }
+// }
 
-function getSum(num1, num2) { //Rure function
-    let sum = 0;
-        for (let i = num1; i <= num2; i++) {
-            sum = sum + i;
+// function getSum(num1, num2) { //Rure function
+//     let sum = 0;
+//         for (let i = num1; i <= num2; i++) {
+//             sum = sum + i;
+//         }
+//         return sum;
+// }
+
+// *********
+
+// let str ='*';
+
+// for (let i=0; i < 9; i++) {
+//     str+='*';
+// }
+
+// console.log(str);
+
+// let str ='';
+// for (let j =0; j < 8; j++ ) {
+//     for (let i=0; i < 8; i++) {
+//     str+='* ';
+//     }
+//     str += '\n'
+// }
+
+// console.log(str);
+
+// Квадрат 
+
+// let str1 = '';
+// for (let k = 0; k <= 3; k++) {
+//     for (let i = 1; i <= 3; i++) {
+//         str1 += i;
+//     }
+//     str1 += '\n';
+// }
+
+// console.log(str1)
+
+// Task
+
+// let num1 = Number(prompt('введить число'));
+// let num2 = Number(prompt('введить друге число'));
+
+// let str3 = '';
+// for (let j = 0; j < num2; j++){
+//     for (let i = 1; i <= num1; i++) {
+//         str3 += '@ ';
+//     }
+//     str3 += '\n';
+// }
+// console.log(str3);
+
+//  Квадрат 
+
+//  let userPrompt = Number(prompt('Ведіть розмір фігури'));
+
+//  drawSquare(userPrompt);
+
+//  function drawSquare(dimension) {
+//         let str3 = '';
+//     for (let j = 0; j < dimension; j++){
+//         for (let i = 0; i < dimension; i++) {
+//             str3 += '@ ';
+//         }
+//         str3 += '\n';
+//     }
+//     console.log(str3);
+//  }
+
+ // Пустий квадрат
+
+  let userEmptyPrompt = Number(prompt('Ведіть розмір фігури'));
+
+ drawEmptySquare(userEmptyPrompt);
+
+ function drawEmptySquare(dimension1) {
+        let str3 = '';
+    for (let j = 0; j < dimension1; j++){
+        for (let i = 0; i < dimension1; i++) {
+            if (j === 0 || j === dimension1 - 1 || i === 0 || i === dimension1 - 1) {  // якщо поточна ітерація циклу - це рамка, то малюємо зірочку
+                str3 += '@ ';
+            } else {  // інакше - малюємо проюіл
+                str3 += '  ';
+            }            
         }
-        return sum;
-}
+        str3 += '\n';
+    }
+    console.log(str3);
+ }
